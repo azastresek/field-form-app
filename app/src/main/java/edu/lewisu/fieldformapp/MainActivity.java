b3f382b2622231a0affc0c2832253b6682f8dd13
andrewmontoyaiv <andrewmontoyaiv@users.noreply.github.com>
2018-10-29 00:51:19 -0500

    connecting main activity with database

--- app/src/main/java/edu/lewisu/fieldformapp/MainActivity.java
+++ app/src/main/java/edu/lewisu/fieldformapp/MainActivity.java
@@ -1,13 +1,46 @@
 package edu.lewisu.fieldformapp;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstName,Address;
+    edu.lewisu.fieldformapp.SQLDatabase sql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstName=(EditText)findViewById(R.id.editText);
+        Address=(EditText)findViewById(R.id.editText2);
+        sql=new edu.lewisu.fieldformapp.SQLDatabase(MainActivity.this);
+    }
+
+    public void click(View v)
+    {
+        String fName = firstName.getText().toString();
+        String add = Address.getText().toString();
+        sql.open();
+        sql.save();
+        sql.close();
+        Toast.makeText(MainActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void view(View v)
+    {
+        String txt=sql.get();
+        Dialog d=new Dialog(MainActivity.this);
+        d.setTitle("Form Data");
+        TextView tv=new TextView(MainActivity.this);
+        tv.setText(txt);
+        d.setContentView(tv);
+        d.show();
     }
 }
---
+++ app/src/main/java/edu/lewisu/fieldformapp/SQLDatabase.java
@@ -0,0 +1,110 @@
+package edu.lewisu.fieldformapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLDatabase {
+    String database="db";
+    String table="userForm";
+    int version=1;
+    String firstName="First Name";
+    String middleName="Middle Name";
+    String lastName="Last Name";
+    String dateOfBirth="Date Of Birth";
+    String SSN="Social Security Number";
+    String email="Email Address";
+    String phoneNum="Phone Number";
+    String contactPref="Contact Preference";
+    String Address="Address";
+    String City="City";
+    String State="State";
+    String Zip="Zip Code";
+    String County="County";
+    String Gender="Gender";
+    String Ethnicity="Ethnicity";
+    String Consent ="Consent";
+    String highSchool="High School";
+    String programOfInterest="Program of Interest";
+    String gradYear="High School Graduation Year";
+    String extraCurricularActivity="Extra Curricular Activities";
+    String Hobbies="Hobbies";
+    String Scholarships="Scholarships";
+    String finanAid="Financial Aid";
+    String medInfo="Medical Information";
+    String id="ID";
+
+    helper h;
+    Context c;
+
+    SQLiteDatabase s;
+
+    public class helper extends SQLiteOpenHelper {
+        public helper(Context context) {
+            super(context, database, null, version);
+            // TODO Auto-generated constructor stub
+        }
+    }
+
+        @Override
+        public void onCreate(SQLiteDatabase arg0) {
+            String query = "Create Table" "+table+" ("+id" INTEGER PRIMARY KEY AUTOINCREMENT,
+            "+firstName+" TEXT NOT NULL);";
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase arg0, int arg1, arg2) {
+            arg0.execSQL(query);
+            {
+                // TODO Auto-generated method stub
+
+            }
+        }
+
+    public SQLDatabase(MainActivity mainActivity)
+    {
+        c=mainActivity;
+        // TODO Auto-generated constructor stub
+    }
+
+    public void open()
+    {
+        h=new helper(c);
+        s=h.getWritableDatabase();
+        // TODO Auto-generated method stub
+    }
+
+    public void save(String na, String ad)
+    {
+        // TODO Auto-generated method stub
+        ContentValues cv=new ContentValues();
+        cv.put(firstName, na);
+        cv.put(Address, ad);
+        s.insert(table, null, cv);
+    }
+
+    public void close()
+    {
+        s.close();
+        // TODO Auto-generated method stub
+    }
+
+    public String get()
+    {
+        h=new helper(c);
+        s=h.getReadableDatabase();
+        String txt="";
+        String[] col={id,firstName,Address};
+        Cursor c=s.query(table, col, null, null, null, null, null);
+        c.moveToFirst();
+        for(c.moveToFirst();!((Cursor) c).isAfterLast();c.moveToNext())
+        {
+            txt=txt+c.getString(0)+" "+c.getString(1)+ " "+c.getString(2)+"\n";
+        }
+        return txt;
+    }
+}
+
